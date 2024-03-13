1c8d376085e2fc20fef9fdd2314fc90a71946add
Carlos Motta <carlos.motta.business@gmail.com>
2024-03-13 21:22:41 +0900

    Add basic vue template

---
+++ book-journal/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/library">Library</RouterLink>
+      </nav>
+    </div>
+
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ book-journal/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="home"></div>
+  </main>
+</template>
