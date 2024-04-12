be4d12346a9a3055a226f292b46f4e518260d9b9
Carlos Motta <carlos.motta.business@gmail.com>
2024-04-12 22:11:19 +0900

    Add books PUT endopoint

--- api/main.go
+++ api/main.go
@@ -13,19 +13,26 @@ type book struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Notes       string `json:"notes"`
-}
-
-type bookResponse struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Notes       string `json:"notes"`
+	Reading     bool   `json:"reading"`
 }
 
 var library = []book{
-	{Id: "1", Title: "Awesome book 1", Description: "This book is cool 1"},
+	{Id: "1", Title: "Awesome book 1", Description: "This book is cool 1", Reading: false},
 }
 
 func getBooks(c *gin.Context) {
+	reading := c.Query("reading")
+	if reading == "true" {
+		readingBooks := make([]book, 0)
+		for _, book := range library {
+			if book.Reading {
+				readingBooks = append(readingBooks, book)
+			}
+		}
+		c.IndentedJSON(http.StatusOK, readingBooks)
+		return
+	}
+
 	c.IndentedJSON(http.StatusOK, library)
 }
 
@@ -46,6 +53,13 @@ func createBook(c *gin.Context) {
 	if err := c.BindJSON(&newBook); err != nil {
 		log.Fatal(err)
 		return
+	}
+
+	type bookResponse struct {
+		Title       string `json:"title"`
+		Description string `json:"description"`
+		Notes       string `json:"notes"`
+		Reading     bool   `json:"reading"`
 	}
 
 	newBook.Id = fmt.Sprint(len(library) + 1)
@@ -77,6 +91,22 @@ func deleteItem(slice []book, index int) []book {
 	return append(newSlice, slice[index+1:]...)
 }
 
+func updateBook(c *gin.Context) {
+	var parsedBook book
+	if err := c.BindJSON(&parsedBook); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for i, item := range library {
+		if item.Id == parsedBook.Id {
+			library[i] = parsedBook
+		}
+	}
+
+	c.IndentedJSON(http.StatusOK, struct{}{})
+}
+
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
@@ -100,6 +130,7 @@ func main() {
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", getBook)
 	router.POST("/books", createBook)
+	router.PUT("/books", updateBook)
 	router.DELETE("/books/:id", deleteBook)
 	router.Run()
 }
